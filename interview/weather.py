fe85e29b827d1b40d6741dc45469ad22a134089c
RieunLinKim <rieun.lin.kim@gmail.com>
2024-10-31 19:24:40 -0500

    set up

--- interview/weather.py
+++ interview/weather.py
@@ -1,6 +1,14 @@
 from typing import Any, Iterable, Generator
-
+from collections import deque
 
 def process_events(events: Iterable[dict[str, Any]]) -> Generator[dict[str, Any], None, None]:
+    responses = deque()
     for line in events:
+        if set(line.keys()) == {"type", "stationName", "timestamp", "temperature"}: # sample message
+            responses.append(line)
+                
+        elif set(line.keys()) == {"type", "command"}:   # control message
+        
+
+
         yield line
