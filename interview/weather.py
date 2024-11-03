ac555d3f61008eb18ea2d26bc4bbce97d96d44f1
RieunLinKim <rieun.lin.kim@gmail.com>
2024-11-02 19:31:18 -0500

    passed 3 DIY tests

--- interview/weather.py
+++ interview/weather.py
@@ -1,14 +1,46 @@
-from typing import Any, Iterable, Generator
-from collections import deque
+from typing import Any, Iterable, Generator, Dict
 
 def process_events(events: Iterable[dict[str, Any]]) -> Generator[dict[str, Any], None, None]:
-    responses = deque()
+    record: Dict[str, Any] = {}
+    curr_timestamp = None
+
     for line in events:
-        if set(line.keys()) == {"type", "stationName", "timestamp", "temperature"}: # sample message
-            responses.append(line)
-                
-        elif set(line.keys()) == {"type", "command"}:   # control message
-        
+        message_type = line["type"]
+        if message_type == "sample": # sample message
+            station_name = line["stationName"]
+            timestamp = line["timestamp"]
+            temperature = line["temperature"]
 
+            if curr_timestamp is None or timestamp > curr_timestamp:
+                # record for new/later timestamp
+                curr_timestamp = timestamp
 
-        yield line
+            if station_name not in record:   # first record of the station
+                record[station_name] = {}
+                record[station_name]["high"] = line["temperature"]
+                record[station_name]["low"] = line["temperature"]
+
+            else : # station record already exists
+                if temperature > record[station_name]["high"] :
+                    record[station_name]["high"] = temperature # new high temperature
+                elif temperature < record[station_name]["low"] :
+                    record[station_name]["low"] = temperature  # new low temperature
+
+        elif message_type == "control":   # control message
+            if not record or curr_timestamp is None: # no existing record
+                continue    # ignore
+
+            output = {}
+            output["type"] = line["command"]
+            output["asOf"] = curr_timestamp
+
+            if line["command"] == "snapshot":   # control message type 1 : snapshot
+                output["stations"] = record.copy()
+
+            elif line["command"] == "reset":  # control message type 2 : reset
+                record = {}
+
+            yield output
+
+        else:
+            raise ValueError(f"input: {message_type} is an unknown message type")
--- interview/weather_test.py
+++ interview/weather_test.py
@@ -1,4 +1,78 @@
 from . import weather
 
-def test_replace_me():
-    assert [{}] == list(weather.process_events([{}]))
+def test_empty_input():
+    assert not list(weather.process_events([]))
+
+def test_regular_input():   # expected sample and control messages
+    sample_message1 = {
+                    "type": "sample",
+                    "stationName": "Foster Weather Station",
+                    "timestamp": 1672531199999,
+                    "temperature": 37.1
+                    }
+
+    sample_message2 = {
+                    "type": "sample",
+                    "stationName": "Foster Weather Station",
+                    "timestamp": 1672531200000,
+                    "temperature": 32.5
+                    }
+
+    snapshot_message1 = {
+                        "type": "control", 
+                        "command": "snapshot"
+                        }
+
+    snapshot_output1 = {
+                        "type": "snapshot",
+                        "asOf": 1672531200000,
+                        "stations": {
+                                    "Foster Weather Station": {"high": 37.1, "low": 32.5}
+                                    }
+                        }
+
+    reset_message = {
+                    "type": "control", 
+                    "command": "reset"
+                    }
+
+    reset_output = {
+                    "type": "reset",
+                    "asOf": 1672531200000
+                    }
+
+    sample_message3 = {
+                "type": "sample",
+                "stationName": "Foster Weather Station",
+                "timestamp": 1672531200001,
+                "temperature": 39
+                }
+
+    snapshot_message2 = {
+                        "type": "control", 
+                        "command": "snapshot"
+                        }
+
+    snapshot_output2 = {
+                        "type": "snapshot",
+                        "asOf": 1672531200001,
+                        "stations": {
+                                    "Foster Weather Station": {"high": 39, "low": 39}
+                                    }
+                        }
+
+    test_input = [sample_message1,
+                sample_message2,
+                snapshot_message1,
+                reset_message,
+                sample_message3,
+                snapshot_message2]
+
+    assert [snapshot_output1, reset_output, snapshot_output2] \
+            == list(weather.process_events(test_input))
+
+def test_unknown_message():
+    try:
+        weather.process_events([{}])
+    except ValueError as e:
+        print(f"The unknown message error was successfully caught: {e}")
